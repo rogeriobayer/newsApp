149fc9997e8fd4afcf8284fad11e77aac90ceb28
jessikatironi <jessikatironi@hotmail.com>
2021-12-12 11:49:54 -0300

    alteracao app.js e criacao context

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import HomeScreen from "./src/screens/HomeScreen";
 import BookmarkedScreen from "./src/screens/BookmarkedScreen";
 import SearchScreen from "./src/screens/SearchScreen";
 import NewsDetailsScreen from "./src/screens/NewsDetailsScreen";
+import { NewsProvider } from "./src/context/NewsContext";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -28,41 +29,43 @@ const HomeStackScreen = () => {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            tabBarActiveTintColor: '#2196f3',
-            tabBarLabelStyle: {
-              fontSize: 15,
-            }
-          }}
-      >
-        <Tab.Screen name="Home" component={HomeStackScreen} 
-            options={{
-              tabBarLabel: 'Headlines',
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="newspaper-outline" color={color} size={size} />
-              ),
+    <NewsProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              tabBarActiveTintColor: '#2196f3',
+              tabBarLabelStyle: {
+                fontSize: 15,
+              }
             }}
-        />
-        <Tab.Screen name="Search" component={SearchScreen}
-            options={{
-              tabBarLabel: 'Search',
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="search-sharp" color={color} size={size} />
-              ),
-            }}
-        />
-        <Tab.Screen name="Bookmarked" component={BookmarkedScreen} 
-            options={{
-              tabBarLabel: 'Bookmarked',
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="heart" color={color} size={size} />
-              ),
-            }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+        >
+          <Tab.Screen name="Home" component={HomeStackScreen} 
+              options={{
+                tabBarLabel: 'Headlines',
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="newspaper-outline" color={color} size={size} />
+                ),
+              }}
+          />
+          <Tab.Screen name="Search" component={SearchScreen}
+              options={{
+                tabBarLabel: 'Search',
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="search-sharp" color={color} size={size} />
+                ),
+              }}
+          />
+          <Tab.Screen name="Bookmarked" component={BookmarkedScreen} 
+              options={{
+                tabBarLabel: 'Bookmarked',
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="heart" color={color} size={size} />
+                ),
+              }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </NewsProvider>
   );
 }
---
+++ src/context/NewsContext.js
@@ -0,0 +1,41 @@
+import React, { useReducer, createContext } from 'react';
+
+const NewsContext = createContext();
+
+const init = { saved: [] };
+
+const newsReducer = (state, action) => {
+    switch (action.type) {
+        case "add":
+            return { ...state, saved: [ ...state.saved, action.payload] };    //insere nova noticia ao final da lista
+        case "delete": {
+            const newArray = state.saved.filter(bookmark => bookmark.title !== action.payload.title);   //filtra vetor do contexto em um vetor auxiliar
+            return{                                                                                     //removendo elemento cujo id == id passado para contexto
+                ...state, 
+                saved: newArray                                                                         //substitui vetor do contexto
+            }       
+        }
+        default:
+            return { ...state}; //retorna contexto
+    }
+};
+
+const NewsProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(newsReducer, init);
+
+    const add = (s, a, t, u, i, d, c) => {
+        dispatch({ type: "add", payload: { source: s, author: a, title: t, url: u, image: i, date: d, content: c} });
+    };
+
+    const remove = (title) => {
+        dispatch({ type: "delete", payload: title});
+    };
+
+    return(
+        <NewsContext.Provider value={{ state, add, remove}}>
+            {children}
+        </NewsContext.Provider>
+    )
+};
+
+export { NewsContext, NewsProvider };
