8dc644c0ca6fc63bd55caa76e7bd591fb7461edf
Rogério Bayer <rogerio.bayer@ticto.com.br>
2021-12-15 17:59:56 -0300

    feat: :sparkles: Nova Login e logo

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { navigationRef } from './RootNavigation';
-import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { navigationRef } from "./RootNavigation";
+import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "./src/screens/HomeScreen";
 import BookmarkedScreen from "./src/screens/BookmarkedScreen";
 import SearchScreen from "./src/screens/SearchScreen";
@@ -39,52 +39,62 @@ const BookmarkStackScreen = () => {
 const Feed = () => {
   return (
     <Drawer.Navigator initialRouteName="Home" headerMode="none">
-      <Drawer.Screen name="Inicio" options={{ drawerInactiveTintColor: 'red' }} component={Inicio} />
-      <Drawer.Screen name="Log out"  component={Logout} />
+      <Drawer.Screen
+        name="Inicio"
+        options={{ drawerInactiveTintColor: "red" }}
+        component={Inicio}
+      />
+      <Drawer.Screen name="Sair" component={Logout} />
     </Drawer.Navigator>
   );
-}
+};
 
-const Inicio = ({ }) => {
+const Inicio = ({}) => {
   return (
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          tabBarActiveTintColor: '#2196f3',
-          tabBarLabelStyle: {
-            fontSize: 15,
-          }
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: "#2196f3",
+        tabBarLabelStyle: {
+          fontSize: 15,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: "Headlines",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="newspaper-outline" color={color} size={size} />
+          ),
+          headerShown: false,
         }}
-      >
-        <Tab.Screen name="Home" component={HomeStackScreen}
-          options={{
-            tabBarLabel: 'Headlines',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="newspaper-outline" color={color} size={size} />
-            ),
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen name="Search" component={SearchScreen}
-          options={{
-            tabBarLabel: 'Search',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="search-sharp" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen name="Saved" component={BookmarkStackScreen}
-          options={{
-            tabBarLabel: 'Bookmarked',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="heart" color={color} size={size} />
-            ),
-            headerShown: false,
-          }}
-        />
-      </Tab.Navigator>
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-sharp" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Saved"
+        component={BookmarkStackScreen}
+        options={{
+          tabBarLabel: "Bookmarked",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
   );
-}
+};
 
 export default function App() {
   return (
@@ -92,10 +102,18 @@ export default function App() {
       <AuthProvider>
         <NavigationContainer ref={navigationRef}>
           <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Feed" component={Feed}
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
               options={{
-                headerShown: false
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Feed"
+              component={Feed}
+              options={{
+                headerShown: false,
               }}
             />
           </Stack.Navigator>
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,97 +1,150 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Button,} from "react-native-elements";
-import { Text, View, StyleSheet, Image, } from 'react-native';
+import React, { useState, useEffect, useContext } from "react";
+import { Button } from "react-native-elements";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableHighlight,
+} from "react-native";
 import { AuthContext } from "../context/AuthContext.js";
-import * as WebBrowser from 'expo-web-browser';
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import * as WebBrowser from "expo-web-browser";
+import { Feather } from "@expo/vector-icons";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
 
 // Endpoint
 const discovery = {
-    authorizationEndpoint: 'https://www.reddit.com/api/v1/authorize.compact',
-    tokenEndpoint: 'https://www.reddit.com/api/v1/access_token',
+  authorizationEndpoint: "https://www.reddit.com/api/v1/authorize.compact",
+  tokenEndpoint: "https://www.reddit.com/api/v1/access_token",
 };
 
 const LoginScreen = ({ navigation }) => {
+  const { authState, signIn, signOut, tryLocalSignIn } =
+    useContext(AuthContext);
 
-    const { authState, signIn, signOut, tryLocalSignIn } = useContext(AuthContext);
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: "FrPIqLo2HyPeDlnJ5kKtxA",
+      scopes: ["identity"],
+      responseType: "token",
+      // For usage in managed apps using the proxy
+      redirectUri: makeRedirectUri({}),
+    },
 
-    const [request, response, promptAsync] = useAuthRequest(
-        {
-            clientId: 'FrPIqLo2HyPeDlnJ5kKtxA',
-            scopes: ['identity'],
-            responseType: 'token',
-            // For usage in managed apps using the proxy
-            redirectUri: makeRedirectUri({}),
-        },
-        
-        discovery
-    );
+    discovery
+  );
 
-    useEffect(() => {
-        console.log(response);
-        tryLocalSignIn();
-    }, []);
+  useEffect(() => {
+    console.log(response);
+    tryLocalSignIn();
+  }, []);
 
-    useEffect(() => {
-        if (response?.type === 'success') {
-            const { code } = response.params;
-            const token = response.authentication.accessToken;
-            console.log(response);
-            signIn(token);
-        }
-    }, [response]);
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { code } = response.params;
+      const token = response.authentication.accessToken;
+      console.log(response);
+      signIn(token);
+    }
+  }, [response]);
 
-    return (
-        <View style={styles.container}>
-            <Button
-                disabled={!request}
-                title="Login Reddit"
-                onPress={() => {
-                    promptAsync();
-                }}
+  function nonSignedLogin() {
+    signIn("");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.greetings}>Bem vindo ao</Text>
+        <Image
+          source={require("../../assets/splash.png")}
+          style={{ width: 200, height: 100, resizeMode: "contain" }}
+        />
+      </View>
+      <View style={styles.container}>
+        <TouchableHighlight
+          onPress={() => {
+            promptAsync();
+          }}
+        >
+          <View style={styles.buttonReddit}>
+            <Feather
+              style={styles.starIcon}
+              name="gitlab"
+              size={16}
+              color="#ffffff"
             />
-            {authState.error ? <Text>{authState.error}</Text> : null}
+            <Text style={styles.login}>Faça login com o Reddit</Text>
+          </View>
+        </TouchableHighlight>
 
-        </View>
-
-
-    );
-}
+        <TouchableHighlight
+          onPress={() => {
+            nonSignedLogin();
+          }}
+        >
+          <View style={styles.buttonLogin}>
+            <Feather
+              style={styles.starIcon}
+              name="user"
+              size={16}
+              color="#ffffff"
+            />
+            <Text style={styles.login}>Entre sem login</Text>
+          </View>
+        </TouchableHighlight>
+        {authState.error ? (
+          <Text style={styles.error}>{authState.error}</Text>
+        ) : null}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    AppTitle: {
-        marginVertical: 50,
-        flex: 0,
-        fontSize: 30,
-        textAlign: 'center',
-        alignItems: 'center',
-        fontWeight: 'bold',
-    },
-    logo: {
-        width: 90,
-        height: 90,
-        margin: 10,
-    },
-    input: {
-        color: 'black',
-        width: '80%',
-        margin: 20,
-        padding: 10,
-        backgroundColor: 'lightgrey',
-    },
-    createButton: {
-        margin: 20,
-        padding: 10,
-        alignSelf: 'center',
-        alignItems: 'center',
-        width: '80%',
-        backgroundColor: '#1f75cb',
-        borderRadius: 15,
-    },
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  AppTitle: {
+    marginVertical: 50,
+    flex: 0,
+    fontSize: 30,
+    textAlign: "center",
+    alignItems: "center",
+    fontWeight: "bold",
+  },
+  greetings: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: "#5968e4",
+  },
+  error: {
+    fontWeight: "bold",
+    fontSize: 15,
+    margin: 10,
+    color: "#f80000",
+  },
+  login: {
+    fontWeight: "bold",
+    fontSize: 15,
+    marginLeft: 10,
+    color: "#ffffff",
+  },
+  buttonReddit: {
+    backgroundColor: "#FF5700",
+    padding: 10,
+    height: 40,
+    flexDirection: "row",
+  },
+  buttonLogin: {
+    backgroundColor: "#1DA1F2",
+    padding: 10,
+    marginTop: 10,
+    height: 40,
+    flexDirection: "row",
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+});
 
 export default LoginScreen;
