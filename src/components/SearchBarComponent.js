7241047b0cb61996ae68ec41210f610edcd2a63a
Rogério Bayer <rogerio.bayer@ticto.com.br>
2021-12-16 11:09:36 -0300

    fix: :art: Remoção de prop desnecessaria

--- src/components/SearchBarComponent.js
+++ src/components/SearchBarComponent.js
@@ -2,32 +2,32 @@ import * as React from "react";
 import { SearchBar } from "react-native-elements";
 
 const SearchBarComponent = ({ value, onTextChange, onTextSubmit }) => {
-  
   return (
     <SearchBar
       platform="android"
       containerStyle={{
-        borderWidth: 1, borderColor: 'gray', borderRadius: 8,
+        borderWidth: 1,
+        borderColor: "gray",
+        borderRadius: 8,
         marginHorizontal: 15,
-        height: 45, justifyContent: 'center'
+        height: 45,
+        justifyContent: "center",
       }}
       inputContainerStyle={{}}
       inputStyle={{}}
       leftIconContainerStyle={{}}
       rightIconContainerStyle={{}}
       loadingProps={{}}
-      onClearText={() => console.log('')}
       placeholder="What are you looking for?"
       placeholderTextColor="#888"
       cancelButtonTitle="Cancel"
       cancelButtonProps={{}}
-      onCancel={() => console.log('')}
       value={value}
-      onChangeText={newText => onTextChange(newText)}
+      onChangeText={(newText) => onTextChange(newText)}
       onEndEditing={() => onTextSubmit(value)}
       onBlur={() => onTextSubmit(value)}
     />
   );
-}
+};
 
 export default SearchBarComponent;
